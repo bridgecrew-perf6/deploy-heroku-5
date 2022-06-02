83341519b0a3614bb7b10242ad336fd10abbfb72
Matheus <matheuscarretta@gmail.com>
2022-06-02 15:48:50 -0300

    teste

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+
+app.get('/', (req, res) => res.send(`<h1> Executando naa porta: ${port} </h1>`));
 app.listen(port, () => console.log(`Online: ${port}`));
